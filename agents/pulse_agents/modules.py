a9d936d52c45ec51feabb7a8e4c6c6bc0694f199
BinaryDrag0n <54193028+georgew79@users.noreply.github.com>
2024-02-01 22:48:25 -0500

    Discretized input from image to array

--- agents/pulse_agents/modules.py
+++ agents/pulse_agents/modules.py
@@ -5,227 +5,268 @@
 
 import torch
 import torch.nn as nn
-from torch.distributions import Categorical
+from torch.distributions import Beta
+from base.modules.intrinsic_motivation import IntrinsicMotivationModule
+
+
+class Policy(nn.Module):
+    def __init__(self, env, hidden_size, a_range=None, state_size=None, goal_size=None, action_size=None):
+        super().__init__()
+        self.a_range = env.action_range if a_range is None else a_range
+        self.state_size = env.state_size if state_size is None else state_size
+        self.goal_size = env.goal_size if goal_size is None else goal_size
+        self.action_size = env.action_size if action_size is None else action_size
+
+        input_size = self.state_size + self.goal_size
+
+        self.layers = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, self.action_size),
+        )
+
+    def forward(self, s, g):
+        """Produce an action"""
+        return torch.tanh(self.layers(torch.cat([s, g], dim=1)) * 0.005) * self.a_range
 
 
 class StochasticPolicy(nn.Module):
-    def __init__(self, w):
+    def __init__(self, env, hidden_size, a_range=None, state_size=None, goal_size=None, action_size=None):
         super().__init__()
-        self.w = int(w)
+        self.a_range = env.action_range if a_range is None else a_range
+        self.state_size = env.state_size if state_size is None else state_size
+        self.goal_size = env.goal_size if goal_size is None else goal_size
+        self.action_size = env.action_size if action_size is None else action_size
 
-        self.conv_layers = nn.Sequential(
-            nn.Conv2d(3, 32, 3, padding=1),
-            #nn.BatchNorm2d(32, affine=False, momentum=0.01),
+        input_size = self.state_size + self.goal_size
+
+        self.layers = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
             nn.ReLU(),
-            nn.Conv2d(32, 32, 3),
-            #nn.BatchNorm2d(32, affine=False, momentum=0.01),
+            nn.Linear(hidden_size, hidden_size),
             nn.ReLU(),
-            nn.Conv2d(32, 32, 3, padding=1),
-            #nn.BatchNorm2d(32, affine=False, momentum=0.01),
+            nn.Linear(hidden_size, hidden_size),
             nn.ReLU(),
-            nn.Conv2d(32, 64, 3),
-        )
-        self.fc_layers = nn.Sequential(
-            nn.LayerNorm(64),
-            nn.Linear(64, 64),
-            nn.ReLU(),
-            nn.Linear(64, 9),
+            nn.Linear(hidden_size, self.action_size * 2),
+            nn.Softplus()
         )
 
-        self.softmax_out = nn.Softmax(dim=1)
-
-    def action_stats(self, s, g, mask):
-        """Get the action distribution statistics"""
+    def action_stats(self, s, g):
         x = torch.cat([s, g], dim=1)
-        x = self.conv_layers(x)
-        x = torch.max(torch.max(x, -1)[0], -1)[0]
-        x = self.fc_layers(x)
-        x = x.masked_fill(mask, -10000.0)
-        return self.softmax_out(x)
+        action_stats = self.layers(x) + 1.05 #+ 1e-6
+        return action_stats[:, :self.action_size], action_stats[:, self.action_size:]
 
-    def forward(self, s, g, mask, greedy=False, action=None):
+    def scale_action(self, logit):
+        # Scale to [-1, 1]
+        logit = 2 * (logit - 0.5)
+        # Scale to the action range
+        action = logit * self.a_range
+        return action
+
+    def action_mode(self, s, g):
+        c0, c1 = self.action_stats(s, g)
+        action_mode = (c0 - 1) / (c0 + c1 - 2)
+        return self.scale_action(action_mode)
+
+    def forward(self, s, g, greedy=False, action_logit=None):
         """Produce an action"""
-        action_probs = self.action_stats(s, g, mask)
-        m = Categorical(action_probs)
+        c0, c1 = self.action_stats(s, g)
+        action_mode = (c0 - 1) / (c0 + c1 - 2)
+        m = Beta(c0, c1)
 
-        # Sample. (Otherwise, evaluate the action provided as argument)
-        if action is None:
+        # Sample.
+        if action_logit is None:
             if greedy:
-                action = action_probs.argmax().view(-1)
+                action_logit = action_mode
             else:
-                action = m.sample()
+                action_logit = m.sample()
 
-        n_ents = -m.entropy()
-        lprobs = m.log_prob(action)
-        return action, lprobs, n_ents
+            n_ent = -m.entropy().mean()
+            lprobs = m.log_prob(action_logit)
+            action = self.scale_action(action_logit)
+            return action, action_logit, lprobs, n_ent
+
+        # Evaluate the action previously taken
+        else:
+            n_ent = -m.entropy().mean(dim=1)
+            lprobs = m.log_prob(action_logit)
+            action = self.scale_action(action_logit)
+            return lprobs, n_ent, action
 
 
-class Value(nn.Module):
-    def __init__(self, w, use_antigoal, like_q=False):
+class Critic(nn.Module):
+    def __init__(self, env, hidden_size, use_antigoal=False, a_range=None, state_size=None, goal_size=None, action_size=None):
         super().__init__()
-        self.w = int(w)
         self.use_antigoal = use_antigoal
-        self.like_q = like_q
 
-        self.conv_layers = nn.Sequential(
-            nn.Conv2d((4 if self.use_antigoal else 3), 32, 3, padding=1),
-            #nn.BatchNorm2d(32, affine=False, momentum=0.01),
+        self.a_range = env.action_range if a_range is None else a_range
+        self.state_size = env.state_size if state_size is None else state_size
+        self.goal_size = env.goal_size if goal_size is None else goal_size
+        self.action_size = env.action_size if action_size is None else action_size
+
+        input_size = self.state_size + self.goal_size + self.action_size
+        if self.use_antigoal:
+            input_size += self.goal_size
+
+        self.layers = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
             nn.ReLU(),
-            nn.Conv2d(32, 32, 3),
-            #nn.BatchNorm2d(32, affine=False, momentum=0.01),
+            nn.Linear(hidden_size, hidden_size),
             nn.ReLU(),
-            nn.Conv2d(32, 32, 3, padding=1),
-            #nn.BatchNorm2d(32, affine=False, momentum=0.01),
+            nn.Linear(hidden_size, hidden_size),
             nn.ReLU(),
-            nn.Conv2d(32, 64, 3),
-        )
-        self.fc_layers = nn.Sequential(
-            nn.LayerNorm(64),
-            nn.Linear(64, 64),
-            nn.ReLU(),
-            nn.Linear(64, 9 if self.like_q else 1),
+            nn.Linear(hidden_size, 1),
         )
 
-    def q_no_grad(self, s, g, ag=None):
+    def q_no_grad(self, s, a, g, ag=None):
         for p in self.parameters():
             p.requires_grad = False
 
-        q = self(s, g, ag)
+        q = self(s, a, g, ag)
 
         for p in self.parameters():
             p.requires_grad = True
 
         return q
 
+    def forward(self, s, a, g, ag=None):
+        """Produce an action"""
+        if self.use_antigoal:
+            return self.layers(torch.cat([s, a, g, ag], dim=1)).view(-1)
+        else:
+            return self.layers(torch.cat([s, a, g], dim=1)).view(-1)
+
+
+class Value(nn.Module):
+    def __init__(self, env, hidden_size, use_antigoal=False, a_range=None, state_size=None, goal_size=None):
+        super().__init__()
+        self.use_antigoal = use_antigoal
+
+        self.a_range = env.action_range if a_range is None else a_range
+        self.state_size = env.state_size if state_size is None else state_size
+        self.goal_size = env.goal_size if goal_size is None else goal_size
+
+        input_size = self.state_size + self.goal_size
+        if self.use_antigoal:
+            input_size += self.goal_size
+
+        self.layers = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, 1),
+        )
+
     def forward(self, s, g, ag=None):
         if self.use_antigoal:
-            x = torch.cat([s, g, ag], dim=1)
+            return self.layers(torch.cat([s, g, ag], dim=1)).view(-1)
         else:
-            x = torch.cat([s, g], dim=1)
-        x = self.conv_layers(x)
-        x = x.mean(dim=-1).mean(dim=-1)
-        x = self.fc_layers(x)
-        return x if self.like_q else x.view(-1)
+            return self.layers(torch.cat([s, g], dim=1)).view(-1)
 
 
-class IntrinsicCuriosityModule(nn.Module):
-    def __init__(self):
+class IntrinsicCuriosityModule(nn.Module, IntrinsicMotivationModule):
+    def __init__(self, env, hidden_size, state_size=None, action_size=None):
         super().__init__()
 
-        self.action_embedding = nn.Embedding(9, 8)
+        self.state_size = env.state_size if state_size is None else state_size
+        self.action_size = env.action_size if action_size is None else action_size
 
         self.state_embedding_layers = nn.Sequential(
-            nn.Conv2d(2, 32, 3, padding=1),
-            # nn.BatchNorm2d(32, affine=False, momentum=0.01),
+            nn.Linear(self.state_size, hidden_size),
             nn.ReLU(),
-            nn.Conv2d(32, 32, 3),
-            # nn.BatchNorm2d(32, affine=False, momentum=0.01),
+            nn.Linear(hidden_size, hidden_size),
             nn.ReLU(),
-            nn.Conv2d(32, 32, 3, padding=1),
-            # nn.BatchNorm2d(32, affine=False, momentum=0.01),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, 3),
+            nn.Linear(hidden_size, hidden_size),
         )
+
         self.inverse_model_layers = nn.Sequential(
-            nn.Linear(128, 64),
+            nn.Linear(2 * hidden_size, hidden_size),
             nn.ReLU(),
-            nn.Linear(64, 9),
+            nn.Linear(hidden_size, self.action_size),
         )
-        self.inv_model_loss = nn.CrossEntropyLoss()
 
         self.forward_model_layers = nn.Sequential(
-            nn.Linear(64+8, 64),
+            nn.Linear(self.action_size + hidden_size, hidden_size),
             nn.ReLU(),
-            nn.Linear(64, 64),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
         )
-
-    def embed_state(self, state):
-        """Get the action distribution statistics"""
-        x = self.state_embedding_layers(state)
-        x = torch.max(torch.max(x, -1)[0], -1)[0]
-        return self.normalize(x)
 
     @staticmethod
     def normalize(x):
         return x / torch.sqrt(torch.pow(x, 2).sum(dim=-1, keepdim=True))
 
-    def surprisal(self, state, next_state, action):
+    def surprisal(self, episode_batch):
         """Compute surprisal for intrinsic motivation"""
-        cat_states = torch.cat([state, next_state], dim=0)
-        cat_states_emb = self.embed_state(cat_states)
-        state_emb = cat_states_emb[:state.shape[0]]
-        next_state_emb = cat_states_emb[state.shape[0]:]
+        state = episode_batch['state']
+        next_state = episode_batch['next_state']
+        action = episode_batch['action']
+        state_emb = self.normalize(self.state_embedding_layers(state))
+        next_state_emb = self.normalize(self.state_embedding_layers(next_state))
+        next_state_emb_hat = self.normalize(self.forward_model_layers(torch.cat([state_emb, action], dim=1)))
+        return torch.mean(torch.pow(next_state_emb_hat - next_state_emb, 2), dim=1)
 
-        action_emb = self.action_embedding(action)
-
-        next_state_emb_hat = self.normalize(self.forward_model_layers(torch.cat([state_emb, action_emb], dim=1)))
-
-        return torch.mean(torch.pow(next_state_emb_hat - next_state_emb, 2), dim=1).detach()
-
-    def forward(self, state, next_state, action):
+    def forward(self, mini_batch):
         """Compute terms for intrinsic motivation via surprisal (inlcuding losses and surprise)"""
-        cat_states = torch.cat([state, next_state], dim=0)
-        cat_states_emb = self.embed_state(cat_states)
-        state_emb = cat_states_emb[:state.shape[0]]
-        next_state_emb = cat_states_emb[state.shape[0]:]
+        state = mini_batch['state']
+        next_state = mini_batch['next_state']
+        action = mini_batch['action']
+        state_emb = self.normalize(self.state_embedding_layers(state))
+        next_state_emb = self.normalize(self.state_embedding_layers(next_state))
 
-        action_logits = self.inverse_model_layers(torch.cat([state_emb, next_state_emb], dim=1))
-        inv_loss = self.inv_model_loss(action_logits, action).mean()
+        action_hat = self.inverse_model_layers(torch.cat([state_emb, next_state_emb], dim=1))
+        inv_loss = torch.mean(torch.pow(action_hat - action, 2))
 
-        action_emb = self.action_embedding(action)
-        next_state_emb_hat = self.normalize(self.forward_model_layers(torch.cat([state_emb, action_emb], dim=1)))
+        next_state_emb_hat = self.normalize(self.forward_model_layers(torch.cat([state_emb, action], dim=1)))
         fwd_loss = torch.mean(torch.pow(next_state_emb_hat - next_state_emb.detach(), 2))
 
         return inv_loss + fwd_loss
 
 
-class RandomNetworkDistillation(nn.Module):
-    def __init__(self):
+class RandomNetworkDistillation(nn.Module, IntrinsicMotivationModule):
+    def __init__(self, env, hidden_size, state_size=None):
         super().__init__()
 
+        self.state_size = env.state_size if state_size is None else state_size
+
         self.random_network = nn.Sequential(
-            nn.Conv2d(2, 64, 3, padding=1),
-            # nn.BatchNorm2d(32, affine=False, momentum=0.01),
+            nn.Linear(self.state_size, 2*hidden_size),
             nn.ReLU(),
-            nn.Conv2d(64, 64, 3),
-            # nn.BatchNorm2d(32, affine=False, momentum=0.01),
+            nn.Linear(2*hidden_size, 2*hidden_size),
             nn.ReLU(),
-            nn.Conv2d(64, 64, 3, padding=1),
-            # nn.BatchNorm2d(32, affine=False, momentum=0.01),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, 3),
+            nn.Linear(2*hidden_size, hidden_size),
         )
 
         self.distillation_network = nn.Sequential(
-            nn.Conv2d(2, 32, 3, padding=1),
-            # nn.BatchNorm2d(32, affine=False, momentum=0.01),
+            nn.Linear(self.state_size, hidden_size),
             nn.ReLU(),
-            nn.Conv2d(32, 32, 3),
-            # nn.BatchNorm2d(32, affine=False, momentum=0.01),
+            nn.Linear(hidden_size, hidden_size),
             nn.ReLU(),
-            nn.Conv2d(32, 32, 3, padding=1),
-            # nn.BatchNorm2d(32, affine=False, momentum=0.01),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, 3),
+            nn.Linear(hidden_size, hidden_size),
         )
-
-    def embed_feature_map(self, feature_map):
-        """Get the action distribution statistics"""
-        x = torch.max(torch.max(feature_map, -1)[0], -1)[0]
-        return self.normalize(x)
 
     @staticmethod
     def normalize(x):
         return x / torch.sqrt(torch.pow(x, 2).sum(dim=-1, keepdim=True))
 
-    def surprisal(self, state, next_state, action):
+    def surprisal(self, episode_batch):
         """Compute surprisal for intrinsic motivation"""
-        r_state_emb = self.embed_feature_map(self.random_network(next_state))
-        d_state_emb = self.embed_feature_map(self.distillation_network(next_state))
+        next_state = episode_batch['next_state']
+        r_state_emb = self.normalize(self.random_network(next_state))
+        d_state_emb = self.normalize(self.distillation_network(next_state))
         return torch.mean(torch.pow(r_state_emb - d_state_emb, 2), dim=1).detach()
 
-    def forward(self, state, next_state, action):
+    def forward(self, mini_batch):
         """Compute losses for intrinsic motivation via surprisal (inlcuding losses and surprise)"""
-        r_state_emb = self.embed_feature_map(self.random_network(next_state)).detach()
-        d_state_emb = self.embed_feature_map(self.distillation_network(next_state))
+        next_state = mini_batch['next_state']
+        r_state_emb = self.normalize(self.random_network(next_state)).detach()
+        d_state_emb = self.normalize(self.distillation_network(next_state))
         return torch.mean(torch.pow(r_state_emb - d_state_emb, 2))
